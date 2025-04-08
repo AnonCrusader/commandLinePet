0143d91669b3cdfa7177a6f94ef332dae403ddf7
AnonCrusader <113392216+AnonCrusader@users.noreply.github.com>
2025-04-08 10:07:55 -0500

    Update mainPet.py
    Added Threading for stat drain in background :)

--- mainPet.py
+++ mainPet.py
@@ -2,6 +2,7 @@ import os
 import platform
 import threading
 import time
+import random
 
 #--Basic needs Stats--#
 Hunger = 100
@@ -32,6 +33,8 @@ def nameConf():
 
     if nameWrong.lower() in ('yes','y','1'):
         print('this is correct')
+        thread = threading.Thread(target=statDrain, daemon=True)
+        thread.start()
         mainPetFunc()
 
     else:
@@ -46,7 +49,44 @@ def mainPetFunc():
         f'Hunger:{Hunger}\n'
         f'Thirst:{Thirst}\n'
         f'Happiness:{Happiness}\n')
+    
+    intrctOpt = input('What would you like to do?\n1) Feed\n 2) Water\n 3)Pet\n 4)Play\n')
+    
 
+
+
+def statDrain():
+    global Hunger
+    global Thirst
+    global Happiness
+    
+    print('now threading')
+    randStat = random.randint(1,3)
+    randAmt = random.randint(1,5)
+
+    while True:
+        if randStat == 1:
+            if Hunger > 0:
+                Hunger -= randAmt
+                print('drained Hunger')
+            else:
+                Hunger == 0
+
+        if randStat == 2:
+            if Thirst > 0:
+                Thirst -= randAmt
+                print('drained Thirst')                
+            else:
+                Thirst == 0
+        if randStat == 3:
+            if Happiness > 0:
+                Happiness -= randAmt
+                print('Drained Happiness')
+            else:
+                Hunger == 0
+        time.sleep(10)
+        randStat = random.randint(1,3)
+        randAmt = random.randint(1,5)
 
 systemCheck()
 
